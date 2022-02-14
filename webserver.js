83e864747a6e26793aae66254b365fefb9664f2e
coding-to-music <connors.tom@gmail.com>
2022-02-14 04:26:14 +0000

    build

--- webserver.js
+++ webserver.js
@@ -1,53 +1,55 @@
-const express=require('express');
-const app=express();
-const PORT=3200;
+import dotenv from "dotenv";
 
-const AWS = require('aws-sdk');
+const express = require("express");
+const app = express();
 
+const AWS = require("aws-sdk");
 
-app.get('/',(req,res)=>{
+dotenv.config();
 
- 
-   
-    AWS.config.update({
-        accessKeyId: "",
-        secretAccessKey: ""
-      });
+const PORT = process.env.PORT || 3000;
 
-      let s3 = new AWS.S3();
+app.get("/", (req, res) => {
+  AWS.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  });
 
-    
-      async function getImage(){
-          const data =  s3.getObject(
-            {
-                Bucket: '',
-                Key: ""
-              }
-            
-          ).promise();
-          return data;
-        }
+  let s3 = new AWS.S3();
 
-          getImage()
-          .then((img)=>{
-              let startHTML="<html><body></body>";
-              let endHTML="</body></html>";
-              let html=startHTML + "<img src='data:image/png;base64," + encode(img.Body) + "'" + "/>" + endHTML;
-            res.send(html)
-          }).catch((e)=>{
-            res.send(e)
-          })
-     
+  async function getImage() {
+    const data = s3
+      .getObject({
+        Bucket: process.env.AWS_BUCKET,
+        Key: process.env.AWS_BUCKET_KEY,
+      })
+      .promise();
+    return data;
+  }
 
-        function encode(data)
-        {
-            let buf = Buffer.from(data);
-            let base64 = buf.toString('base64');
-            return base64
-        }
+  getImage()
+    .then((img) => {
+      let startHTML = "<html><body></body>";
+      let endHTML = "</body></html>";
+      let html =
+        startHTML +
+        "<img src='data:image/png;base64," +
+        encode(img.Body) +
+        "'" +
+        "/>" +
+        endHTML;
+      res.send(html);
+    })
+    .catch((e) => {
+      res.send(e);
+    });
 
-
-})
-app.listen(PORT,()=>{
-    console.log(`Web Server running on port ${PORT}`);
+  function encode(data) {
+    let buf = Buffer.from(data);
+    let base64 = buf.toString("base64");
+    return base64;
+  }
+});
+app.listen(PORT, () => {
+  console.log(`Web Server running on port ${PORT}`);
 });
